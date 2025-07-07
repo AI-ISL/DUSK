129a0a57492be44c0a327968de33928a7c2fa4dc
hyesoo <hyesoo.hong@yonsei.ac.kr>
2025-07-07 21:39:50 +0900

    update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import torch
+import yaml
 
 def set_random_seed(seed):
     np.random.seed(seed)
@@ -9,3 +10,19 @@ def set_random_seed(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+def get_model_identifiers_from_yaml(model_family):
+    # path is model_configs.yaml
+    '''
+    models:
+        llama2-7b:
+            hf_key: "NousResearch/Llama-2-7b-chat-hf"
+            question_start_tag: "[INST] "
+            question_end_tag: " [/INST] "
+            answer_tag: ""
+            start_of_sequence_token: "<s>"
+    '''
+    model_configs = {}
+    with open("config/model_config.yaml", "r") as f:
+        model_configs = yaml.load(f, Loader=yaml.FullLoader)
+    return model_configs[model_family]
